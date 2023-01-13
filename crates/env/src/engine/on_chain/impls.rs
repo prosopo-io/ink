930c4bdf1b315112773b86f0a8d166aa6bb9ce5d
Chris Taylor <forgetso86@gmail.com>
2023-01-13 19:44:44 +0000

    try using BlockHash in impl

--- crates/env/src/engine/on_chain/impls.rs
+++ crates/env/src/engine/on_chain/impls.rs
@@ -387,7 +387,7 @@ impl TypedEnvBackend for EnvInstance {
     }
 
     fn block_hash<E: Environment>(&mut self) -> E::Hash {
-        self.get_property_inplace::<E::Hash>(ext::block_hash)
+        self.get_property_inplace::<E::BlockHash>(ext::block_hash)
     }
 
     fn account_id<E: Environment>(&mut self) -> E::AccountId {
