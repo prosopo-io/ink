cfd042d97914a1d5ebbc8b61887f3b047f0eccc8
Chris Taylor <forgetso86@gmail.com>
2023-01-13 11:36:25 +0000

    remove Default requirement

--- crates/env/src/engine/on_chain/impls.rs
+++ crates/env/src/engine/on_chain/impls.rs
@@ -386,10 +386,7 @@ impl TypedEnvBackend for EnvInstance {
         self.get_property_little_endian::<E::Timestamp>(ext::now)
     }
 
-    fn block_hash<E: Environment>(&mut self) -> E::Hash
-    where
-        <E as Environment>::Hash: Default,
-    {
+    fn block_hash<E: Environment>(&mut self) -> E::Hash {
         self.get_property_inplace::<E::Hash>(ext::block_hash)
     }
 
